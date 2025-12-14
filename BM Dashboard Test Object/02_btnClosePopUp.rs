93d1fa5d4ba065628de1bbba4072f71d3611124e
denahsm1112 <125862503+denahsm1112@users.noreply.github.com>
2025-12-15 04:42:58 +0700

    Add files via upload

---
+++ BM Dashboard Test Object/02_btnClosePopUp.rs
@@ -0,0 +1,18 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>02_btnClosePopUp</name>
+   <tag></tag>
+   <elementGuidId>ad5dc812-6f63-4b1f-b30f-9ab898edb565</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>/html/body/div[9]/div[3]</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
---
+++ BM Dashboard Test Object/10_btnReceiveOrder.rs
@@ -0,0 +1,25 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>10_btnReceiveOrder</name>
+   <tag></tag>
+   <elementGuidId>3627e095-6978-4098-93d4-27bbe013a9fc</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//*[contains(text(),'Receive Order')]</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <type>Main</type>
+      <value>//*[contains(text(),'Receive Order')]</value>
+      <webElementGuid>99b61874-531f-45c3-9ad1-b110725e613f</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
