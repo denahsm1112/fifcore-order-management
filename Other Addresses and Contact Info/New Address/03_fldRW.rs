11e373a87a3f2b19d9604243a7580323e5955020
Adenawati Hasim <71482@fif.co.id>
2025-12-15 05:28:39 +0700

    coba

--- Other Addresses and Contact Info/New Address/03_fldRW.rs
+++
@@ -1,9 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<WebElementEntity>
-   <description></description>
-   <name>03_fldRW</name>
-   <tag></tag>
-   <elementGuidId>534eb48d-2c10-4b33-b6dc-aadb8696781e</elementGuidId>
-   <selectorMethod>BASIC</selectorMethod>
-   <useRalativeImagePath>false</useRalativeImagePath>
-</WebElementEntity>
